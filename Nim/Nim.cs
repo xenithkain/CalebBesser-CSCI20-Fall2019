4245a28051a13f6e6428d6a8568d32ab2c748d32
xenithkain <cjesterblake101@gmail.com>
2019-09-15 04:04:43 -0700

    9-15-2019: Added comments, casue i forgot

--- Nim/Nim.cs
+++ Nim/Nim.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Caleb Besser
+//9-15-2019
+// This code is the game of Nim, it allowes players to draw beads from two pile, until the piles hit 0. The player to draw the last one, wins.
+using System;
 
 class Game_Of_Nim
 {
@@ -10,11 +13,12 @@ class Game_Of_Nim
         int Pile_1 = 10;
         int Turn = 1;
         int P1_Beads, P1_Pile;
-
+        //This loop keeps the game going untill both pile are to 0
         while (GameIsDone = true)
         {
             switch (Turn)
             {
+                //This is player 1's code, it stops the loop and says player 1 wins if the piles hit zero on his turn
                 case 1:
                     Console.Write("Player 1, which pile do you choose? 1 or 2? ");
                     P1_Pile = Convert.ToInt32(Console.ReadLine());
@@ -28,7 +32,7 @@ class Game_Of_Nim
                     {
                         Pile_2 = Pile_2 - P1_Beads;
                     }                    
-                    if (Pile_1 <= 0)
+                    if (Pile_1 <= 0) //Added to both player 1 and 2, to stop pile count from going into negatives.
                     {
                         Pile_1 = 0;
                     }
@@ -45,8 +49,9 @@ class Game_Of_Nim
                     }
                     Turn = 2;
                     break;
+                //This is Player 2's code, does the same as 1, but the value for its pile and beads choice, is made by random.
                 case 2:
-                    Random rnd = new Random();                    
+                    Random rnd = new Random();  //added to player 2's code, from top, to fix it not creating a new random every loop                  
                     int P2_Beads = rnd.Next(1, 4);
                     int P2_Pile = rnd.Next(1, 3);
 
@@ -60,7 +65,7 @@ class Game_Of_Nim
                     {
                         Pile_2 = Pile_2 - P2_Beads;
                     }                    
-                    if (Pile_1 <= 0)
+                    if (Pile_1 <= 0) 
                     {
                         Pile_1 = 0;
                     }
