a4f98cbce8da06c0f2d8c1aa70aee7f28ea115ee
xenithkain <cjesterblake101@gmail.com>
2019-10-22 19:31:24 -0700

    10-22-2019: Made psuedo code for Function Input.

---
+++ SchoolWork/2.3 Function Input/Conversion.cs
@@ -0,0 +1,33 @@
+﻿//Caleb Besser
+//10-22-2019
+//This code will allow the user to choose a conversion type, then take their input and it will convert it.
+
+
+
+
+//Main function
+//Takes the user input, for which conversion to do, and then what values to convert.
+
+//switch statement (Conversion)
+//Will ask whether they was the conversion one way or the other, by asking what they want to convert to. So if they say Inches, i know itll be Centimeters to Inches.
+//Will call different methods depending on which conversion they choose.
+//Each funtion will have a WriteLine that writes out the output.
+
+
+
+
+//Inches to Centimeters Conversion Method
+//Will take the Input from Main. Inches and Centimeters.
+//Will put the output into a variable and output it.
+
+//Inches to Feet Conversion Method
+//Will take the Input from Main. Inches and Feet.
+//Will put the output into a variable and output it.
+
+//Gallons to Liters Conversion Method
+//Will take the Input from Main. Gallons and Liters.
+//Will put the output into a variable and output it.
+
+//Liter to Ounces Conversion Method
+//Will take the Input from Main. Liters and Ounces.
+//Will put the output into a variable and output it.
---
+++ SchoolWork/2.3 Function Input/How Many Eggs.cs
@@ -0,0 +1,15 @@
+﻿//Caleb Besser
+//10-22-2019
+//This code will take a number of dozens of eggs, then tell you how many eggs that is.
+
+
+//Main Function
+//This function will take the user input of the amount of dozens.
+//Then i will call the Counting function.
+
+
+
+//Counting Function
+//Will take the Input from Main. The number of Dozens.
+//It will then multiply that number by 12 to get the amount of eggs.
+//Then it will Console.WriteLine the output.
