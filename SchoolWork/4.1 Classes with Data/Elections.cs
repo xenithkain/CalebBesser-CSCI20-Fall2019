49e183ca83b624387f77b018ab33142c011d3245
xenithkain <cjesterblake101@gmail.com>
2019-11-07 18:58:01 -0800

    11-7-2019: Did psuedo code for Elections, coded zoo.

---
+++ SchoolWork/4.1 Classes with Data/Elections.cs
@@ -0,0 +1,33 @@
+﻿//Caleb Besser
+//11-7-2019
+//This code will take people election votes and use structs to figure out who won.
+
+
+
+//Struct Vote
+//Each vote will have 3 electorates, and points for each. So a total of 6 traits.
+//I will assign these values in the Main function.
+
+
+
+
+//Main Function
+//This function will take the inputs for the vote from the user. Then it will assign them to the vote struct.
+//Then i will use the count up function to figure out who won, taking the given votes.
+//Then i will output the winner.
+
+
+//Points_Per_Place function
+//This will take the traits of each vote given. Then it will assign each electorate the amount of points they got overall.
+//Then it will output the electorate with the most points.
+//This will be given to the Main function.
+
+//Approval_rating function
+//This will take the traits of each vote given. Then it will check how many times that name was second or first, and gives them a point.
+//Then it adds the points up and outputs that name.
+//This output will be in the main function.
+
+//Winner_Takes_All function
+//This will take the traits of each vote given. Then it will see how many times that name was first, and assign it a point for each.
+//Then it takes whoever has the most and outputs that.
+//This output will be in the main function.
---
+++ SchoolWork/4.1 Classes with Data/Zoo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Zoo
+{
+    public struct Animal
+    {
+        public string Name;
+        public string Species;
+        public int Age;
+        public string Breakfast_Hour;
+        public string Dinner_Hour;
+    }
+    //My main function
+    static void Main()
+    {
+        //Here i make the first animal
+        Animal Tiger = new Animal();
+        Tiger.Name = "Lenny";
+        Tiger.Species = "Tiger";
+        Tiger.Age = 6;
+        Tiger.Breakfast_Hour = "7:00 am";
+        Tiger.Dinner_Hour = "8:00 pm";
+
+        //Writes out the animals.
+        Console.WriteLine("Animals: ");
+        Console.WriteLine("Species: {0}  Name: {1}  Age: {2}  Breakfast: {3}  Dinner: {4}", Tiger.Species, Tiger.Name, Tiger.Age, Tiger.Breakfast_Hour, Tiger.Dinner_Hour);
+        Console.ReadLine();
+    }
+
+}
